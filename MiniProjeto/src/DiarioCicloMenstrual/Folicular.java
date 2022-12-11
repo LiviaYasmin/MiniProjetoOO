1ce2f082217aba3a727f4489b8631fcf2e28cdc8
livia <livia@DESKTOP-BJ49C94>
2022-12-11 00:15:37 -0300

    Adicionar classes Folicular,Ovulatorio, Sintomas e Usuario.

---
+++ MiniProjeto/src/DiarioCicloMenstrual/Folicular.java
@@ -0,0 +1,47 @@
+package DiarioCicloMenstrual;
+
+public class Folicular extends Periodo{
+		//Atributos
+	private int duracaoMenstruacao;
+	private String inicioMenstruacao;
+	
+	
+		//Metodos Especiais
+	public int getDuracaoMenstruacao() {
+		return duracaoMenstruacao;
+	}
+	public void setDuracaoMenstruacao(int duracaoMenstruacao) {
+		this.duracaoMenstruacao = duracaoMenstruacao;
+	}
+	public String getInicioMenstruacao() {
+		return inicioMenstruacao;
+	}
+	public void setInicioMenstruacao(String inicioMenstruacao) {
+		this.inicioMenstruacao = inicioMenstruacao;
+	}
+	
+	
+		//Metodos Publicos
+	public void calculaMenstruacao() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ MiniProjeto/src/DiarioCicloMenstrual/Usuario.java
@@ -0,0 +1,5 @@
+package DiarioCicloMenstrual;
+
+public class Usuario {
+
+}
